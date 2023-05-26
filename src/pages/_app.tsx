4be9c7f9a427dec7921cfb009a74006289329c22
esrichardd <espinozar1994@gmail.com>
2023-05-26 00:47:55 -0400

    feat(main): add viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: any) {
             <Head>
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="manifest" href="/manifest.json" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
                 <meta name="theme-color" content="#ffffff" />
             </Head>
             <div>
