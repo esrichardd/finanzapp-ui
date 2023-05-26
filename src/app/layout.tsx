befc8effc71f7f07301e4a6ac01064d5f1753f29
esrichardd <espinozar1994@gmail.com>
2023-05-26 00:39:35 -0400

    feat(main): add viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
             <body>
                 <NextAuthProvider>{children}</NextAuthProvider>
             </body>
