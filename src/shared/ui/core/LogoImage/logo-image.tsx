d7b8a114a62a23410c20567eb3ac777474dab71d
esrichardd <espinozar1994@gmail.com>
2023-05-26 01:28:16 -0400

    feat(main): code smells

--- src/shared/ui/core/LogoImage/logo-image.tsx
+++ src/shared/ui/core/LogoImage/logo-image.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { LogoImageProps } from '@/shared/utils/core/types'
-import './logo-image.scss'
+
 export function LogoImage({ height = 100, width = 100 }: LogoImageProps) {
     return <Image src="logo-home.svg" alt="Finanzapp" width={height} height={width} />
 }
